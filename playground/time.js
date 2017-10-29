f74f5df1c9a44d55410bf320f11e0727a3fd4ddc
LuisCasas <sc.luis@gmail.com>
2017-10-29 18:53:14 +0000

    Update chat time format with moment

--- playground/time.js
+++ playground/time.js
@@ -4,14 +4,14 @@ const moment = require('moment');
 
 // console.log(date.getMonth());
 
-var date = moment();
-
-date.add(10,'y').subtract(4,'months');
-
-console.log(date.format('MMM YYYY, Do'));
+// var date = moment();
+// date.add(10,'y').subtract(4,'months');
+// console.log(date.format('MMM YYYY, Do'));
 
 // 7:15 pm
+var timestamp = moment().valueOf();
+console.log(timestamp);
 
-var time = moment();
-
-console.log(date.format('h:mm a'));
+var createAt = 5432;
+var time = moment(createAt);
+console.log(time.format('h:mm a'));
